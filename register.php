da12107efe99ae76c734804818f865240f520504
MCatalin420 <maior.catalin420@gmail.com>
2023-01-22 06:41:10 +0200

    Final

--- register.php
+++ register.php
@@ -20,15 +20,49 @@ if(isset($_POST['submit'])){
 $conn->close();
 ?>
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vintage Shop</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
 <html>
-<body>
-<h1 class="titlucufont">Inregistrare</h1>
-<form method="post" action="">
-    Username: <input type="text" name="name"><br>
-    Password: <input type="password" name="password"><br>
-    <br>
-    <input type="submit" name="submit" value="Inregistrare">
-</form>
 
+<body>
+
+    <div class="loginbox d-flex aligns-items-center justify-content-center">
+        <div class="mx-auto">
+            <form method="post" action="">
+            <h1 class="titlu-tabel">Inregistrare</h1>
+                <div class="forminput">
+                    <label>Username:</label>
+                    <input type="text" name="username" placeholder="username">
+                </div>
+
+                <br>
+                <div class="forminput">
+					<label>Password: </label>
+					<input type="password" name="password" placeholder="password">
+				</div>
+
+				<br>
+                <input type="submit" name="submit" value="Inregistrare">
+            </form>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
